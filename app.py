eb41b9a92a4f844beb41c404779c27503ef3a2e2
pentarosarium <denis.pokrovsky@gmail.com>
2024-10-22 16:29:26 +0300

    progress more (3.52)

--- app.py
+++ app.py
@@ -420,7 +420,7 @@ def main():
         unsafe_allow_html=True
     )
     
-    st.title("::: анализ мониторинга новостей СКАН-ИНТЕРФАКС (v.3.5):::")
+    st.title("::: анализ мониторинга новостей СКАН-ИНТЕРФАКС (v.3.52):::")
     
     if 'processed_df' not in st.session_state:
         st.session_state.processed_df = None
